e84ee2c3a53887a2c430eff278a12ca8233ba677
funkiki david <funkiki.tech@gmail.com>
2026-05-08 00:48:39 -0700

    Add shared TypeScript types

---
+++ lib/types.ts
@@ -0,0 +1,41 @@
+export type InventoryItem = {
+  sku: string;
+  name: string;
+  category: string;
+  qty: number;
+};
+
+export type Customer = {
+  name: string;
+  orders: number;
+  total: number;
+  payment: string;
+  salesRep: string;
+  lastOrder: string;
+};
+
+export type SalesOrderLineItem = {
+  sku: string;
+  description: string;
+  width: string;
+  length: string;
+  category: string;
+  qty: number;
+  unitPrice: number;
+  total: number;
+};
+
+export type SalesOrder = {
+  invoice: string;
+  date: string;
+  shipDate: string;
+  customer: string;
+  po: string;
+  payment: string;
+  shipMethod: string;
+  shipCost: string;
+  salesRep: string;
+  items: SalesOrderLineItem[];
+  totalQty: number;
+  subtotal: number;
+};
